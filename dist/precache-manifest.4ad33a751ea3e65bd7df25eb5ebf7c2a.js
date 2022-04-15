85517dd20738c777f7c92b3eac4412202d136807
Sebi Nemeth <sebezhetetlen98@gmail.com>
2022-04-15 15:11:57 +0200

    adding dist subtree

---
+++ dist/precache-manifest.4ad33a751ea3e65bd7df25eb5ebf7c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f3726002bc150412d9",
+    "url": "/datetimeformat/css/chunk-vendors.7f974aba.css"
+  },
+  {
+    "revision": "6b0f1b8d3860a8969d83d51c32c21954",
+    "url": "/datetimeformat/index.html"
+  },
+  {
+    "revision": "a6496a8eeeff1325d9eb",
+    "url": "/datetimeformat/js/app.4cf715ec.js"
+  },
+  {
+    "revision": "85f3726002bc150412d9",
+    "url": "/datetimeformat/js/chunk-vendors.a032d164.js"
+  },
+  {
+    "revision": "097444ba33ad9aaa4aa992936d1da5a9",
+    "url": "/datetimeformat/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/datetimeformat/robots.txt"
+  }
+]);
